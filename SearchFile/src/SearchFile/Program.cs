2401f6652f5ecef200581f5d983f34e7b059584c
se-yoshi <se.yoshi@gmail.com>
2009-10-11 16:50:49 +0000

    不要なusingを削除

--- SearchFile/src/SearchFile/Program.cs
+++ SearchFile/src/SearchFile/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-using MyLib.CustomControls;
 
 namespace SearchFile
 {
--- SearchFile/src/SearchFile/Utils/LinearGradientDrawLabelStateManager.cs
+++ SearchFile/src/SearchFile/Utils/LinearGradientDrawLabelStateManager.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MyLib.CustomControls;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using MyLib.CustomControls;
 
 namespace SearchFile.Utils
 {
